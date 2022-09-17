452a23dcb9c002fd776ca350ed82535750e2691b
Alexey Panfilov <ap83@list.ru>
2022-09-17 21:41:10 +0300

    pre-release version

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,36 @@
+import basket.Basket;
+
+import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[][] products = {{"Молоко", "100"}, {"Крупа", "50"}, {"Чай", "80"}, {"Сахар", "60"}};
+        Basket basket = new Basket(products);
+        File basketTxt = new File("basket.txt");
+        if (basketTxt.exists()) {
+            String[][] strings = Basket.loadFromFile(basketTxt);
+            for (int i = 0; i < products.length; i++) {
+                for (int j = 0; j < strings.length; j++) {
+                    if (strings[j][0].equals(products[i][0])) {
+                        basket.addToCart(i, Integer.parseInt(strings[j][1]));
+                    }
+                }
+            }
+        }
+        else System.out.println("Ранее созданная корзина отсутствует, будет формироваться новая");
         System.out.println();
-        String products[][] = {{"Молоко", "100"}, {"Крупа", "50"}, {"Чай", "80"}, {"Сахар", "60"}};
         int prices[] = {100, 50, 80, 60};
         int productsCount[] = new int[products.length]; // накопительный счетчик продуктов одного типа в корзине
-
         while (true) {
-            System.out.println("Выберите номер продукта из списка ниже и количество через пробел. Для завершения программы и вывода итогов введите end:");
-            for (int i = 0; i < products.length; i++) {
-                System.out.println((i + 1) + ". " + products[i] + " - " + prices[i] + " руб.");
-            }
+            basket.printBasket();
+            System.out.println("Выберите номер продукта из списка и количество через пробел. " +
+                    "Для завершения программы и вывода итогов введите end:");
             String choice = scanner.nextLine();
-            ;
+
             if (choice.equals("end")) {
                 break;
             }
@@ -29,36 +44,25 @@ public class Main {
             int productCount;
             try {
                 productNumber = Integer.parseInt(parts[0]) - 1; //порядковый номер продукта в массиве,
-                productCount = Integer.parseInt(parts[1]);
+                productCount = Integer.parseInt(parts[1]); // количество единиц данного продукта
             } catch (NumberFormatException e) {
                 System.out.println("Вы ввели что-то совсем непонятное");
                 continue;
             }
-            if (parts.length != 2) {
-                System.out.println("Введите корректные значения");
-                continue;
-            }
-            if (productNumber < 0 || productNumber >= products.length) {
+            if (productNumber < 0 || productNumber > products.length) {
                 System.out.println("Выберите порядковый номер в соответствии с представленным списком");
                 continue;
             } else if (productCount < 0) {
                 System.out.println("Мы не можем положить в корзину " + productCount + " " + products[productNumber]);
                 continue;
             } else if (productCount == 0) {
-                //System.out.println("Мы конечно можем представить что положили в корзину невидимый товар, но лучше введите корректные значения");
+                System.out.println("Вы ничего не положили в корзину");
                 //continue;
                 productsCount[productNumber] = 0;
             }
-            productsCount[productNumber] += productCount;
-            System.out.println("Вы положили в корзину " + products[productNumber] + ", " + productCount + ", шт");
+            basket.addToCart(productNumber, productCount);
+            basket.saveTxt(basketTxt);
         }
-        int bill = 0;
-        for (int i1 = 0; i1 < products.length; i1++) {
-            if (productsCount[i1] != 0) {
-                bill += productsCount[i1] * prices[i1];
-                System.out.println(products[i1] + " - " + productsCount[i1] + " шт., " + prices[i1] + " руб/шт., " + productsCount[i1] * prices[i1] + " руб. в сумме");
-            }
-        }
-        System.out.println("Общая сумма покупок: " + bill + " руб.");
+        basket.printCart();
     }
 }
--- src/basket/Basket.java
+++ src/basket/Basket.java
@@ -1,29 +1,90 @@
 package basket;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Basket {
 
-    protected String[][] productsList;
-    protected int[] itemsInCart = new int[productsList.length];
+    protected String[][] productsList; // двумерный массив-список товаров с ценами, доступных к покупке
+    protected int[] itemsInCart; // массив количества каждого товара в корзине
+    protected int bill = 0;
+
 
     public Basket(String[][] productsList) {
         this.productsList = productsList;
+        this.itemsInCart = new int[productsList.length];
+    }
+
+    public void printBasket () {
+        System.out.println("Перечень товаров к покупке:\n");
+        if (productsList.length != 0) {
+            for (int i = 0; i < productsList.length; i++) {
+                System.out.println(productsList[i][0] + ", цена: " + productsList[i][1] + " руб/шт.");
+            }
+        }
     }
 
     public void addToCart(int productNum, int amount) {
-        itemsInCart[productNum] = amount;
+        itemsInCart[productNum] += amount;
+        System.out.println("Вы положили в корзину " + productsList[productNum][0] + ", " + amount + " шт.");
+        bill += Integer.parseInt(productsList[productNum][1]) * amount;
     }
 
     public void printCart() {
-
+        System.out.println("Итоговая корзина покупок:\n");
+        for (int i = 0; i < itemsInCart.length; i++) {
+            if (itemsInCart[i] != 0) {
+                System.out.println(productsList[i][0] + ", " + productsList[i][1] + " руб/шт: "
+                        + itemsInCart[i] + " шт, " + (Integer.parseInt(productsList[i][1]) * itemsInCart[i]) + " руб");
+            }
+        }
+        System.out.println("Общая стоимость: " + bill);
     }
 
-    public File saveTxt(File textFile) {
-
+    public void saveTxt(File textFile) {
+        try (BufferedWriter saveCartToFile = new BufferedWriter(new FileWriter(textFile))) {
+            for (int i = 0; i < itemsInCart.length; i++) {
+                if (itemsInCart[i] != 0) {
+                    // заносим в файл наименование товара из списка
+                    // количество товара берем из массива количества товара в корзине
+                    // в конце строки стоимость товара
+                    saveCartToFile.write((productsList[i][0] + ": " +
+                            itemsInCart[i] + " шт, " +
+                            Integer.parseInt(productsList[i][1]) * itemsInCart[i]) + " руб");
+                    saveCartToFile.append("\n");
+                    saveCartToFile.flush();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static Basket loadFromFile(File textFile) {
-
+    public static String[][] loadFromFile(File textFile) {
+        String dataFromFile = "";
+        try (BufferedReader loadCartFromFile = new BufferedReader(new FileReader(textFile))){
+            // заводим переменную для чтения из файла, т.к. при упоминании метода в скобках
+            // цикла / ифа он выполняется,
+            // итого без переменной у нас каждая вторая итерация цикла останется без данных
+            String s;
+            while (true) {
+                if ((s = loadCartFromFile.readLine()) == null) break;
+                dataFromFile += s + "\n";
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        // я не вспомню как создавался этот пиздец...
+        String[] splitLines = dataFromFile.split("\\n");
+        String[][] productsListFromFile = new String[splitLines.length][2];
+        for (int i = 0; i < splitLines.length; i++) {
+            String[] split = splitLines[i].split(": |, | ");
+            productsListFromFile[i][0] = split[0];
+            productsListFromFile[i][1] = split[1];
+        }
+        System.out.println(Arrays.deepToString(productsListFromFile));
+        return productsListFromFile;
     }
 }
